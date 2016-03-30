110cf3aec737cac534f0fca101b9c4381805c956
Jason DeWitt <jason.dewitt@10up.com>
2016-03-30 16:23:51 -0500

    adding hyperdb

--- wordpress/containerbuddy/reload-db.sh
+++ wordpress/containerbuddy/reload-db.sh
@@ -4,4 +4,4 @@ consul-template \
     -once \
     -dedup \
     -consul ${CONSUL}:8500 \
-    -template "/var/www/html/db-config.php.ctmpl:/var/www/html/wordpress/db-config.php"
+    -template "/var/www/html/db-config.php.ctmpl:/var/www/html/content/db-config.php"

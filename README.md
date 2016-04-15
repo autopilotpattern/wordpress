240a580dea3590feda8bc3254dc868dd1a2bd87d
Jason DeWitt <jason.dewitt@10up.com>
2016-04-15 15:55:48 -0500

    working on the readme setup.sh shamelessly stolen from couchbase repo, needs work

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# triton-wordpress
+# autopilot-wordpress
+*a Docker Compose leveraging services from many containers to create an robust WordPress envrionment*
+
+### Containerized and discoverable via Consul
+This project uses Consul for service discovery, and all component containers announce themselves to Consul and get the information they need about other services from Consul. This allows each container to configure itself once the services it depends on are online. It also allows each service to be scaled up to handle incoming traffic and as more services are added, the containers that consume these services will reconfigure themselves accordingly.
+
+### Project Architecure
+A running cluster includes the following components:
+
+- [Consul](https://www.consul.io/): used to coordinate replication and failover
+- [Autopilot-MySQL](https://github.com/autopilotpattern/mysql/): we're using the Autopilot-MySQL project to leverage the great features built into this container. It users MySQL5.6 via [Percona Server](https://www.percona.com/software/mysql-database/percona-server), and [`xtrabackup`](https://www.percona.com/software/mysql-database/percona-xtrabackup) for running hot snapshots.
+- [Manta](https://www.joyent.com/object-storage): the Joyent object store, for securely and durably storing our MySQL snapshots.
+- [ContainerPilot](http://containerpilot.io): included in our MySQL containers orchestrate bootstrap behavior and coordinate replication using keys and checks stored in Consul in the `onStart`, `health`, and `onChange` handlers.
+- [NFS](https://github.com/autpilotpattern/nfsserver/): Stores user uploaded files so these files can be shared between many WordPress containers and served to the world
+- [Memcached](https://github.com/autpilotpattern/memcached/): Caches often accessed data in memory so WordPress doesn't always have to access the database
+- [Nginx](https://github.com/autopilotpattern/nginx): Front-end load balancer for the WordPress envrionment, passes traffic from users to the WordPress containers on the back-end.
+
+### Getting started
+
+1. [Get a Joyent account](https://my.joyent.com/landing/signup/) and [add your SSH key](https://docs.joyent.com/public-cloud/getting-started).
+1. Install the [Docker Toolbox](https://docs.docker.com/installation/mac/) (including `docker` and `docker-compose`) on your laptop or other environment, as well as the [Joyent Triton CLI](https://www.joyent.com/blog/introducing-the-triton-command-line-tool) (`triton` replaces our old `sdc-*` CLI tools)
+1. [Configure Docker and Docker Compose for use with Joyent](https://docs.joyent.com/public-cloud/api-access/docker):
+
+```bash
+curl -O https://raw.githubusercontent.com/joyent/sdc-docker/master/tools/sdc-docker-setup.sh && chmod +x sdc-docker-setup.sh
+./sdc-docker-setup.sh -k us-east-1.api.joyent.com <ACCOUNT> ~/.ssh/<PRIVATE_KEY_FILE>
+```
+### Joyent Manta configuration
+```
+write this:
+needs ssh keys for manta
+```
+
+
+
+Check that everything is configured correctly by running `./setup.sh`. If it returns without an error you're all set. This script will create and `_env` file that includes the Triton CNS name for the Consul service. You'll want to edit this file to update the username and password for Couchbase.
--- setup.sh
+++ setup.sh
@@ -1,15 +1,138 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e -o pipefail
 
-# run this script to setup your envrionment to start the project
-# add the public and private keys you used in your MANTA setup to
-# manta.id_rsa dn manta.id_rsa.pub or edit this script
+help() {
+    echo 'Usage ./setup.sh [-f docker-compose.yml] [-p project]'
+    echo
+    echo 'Checks that your Triton and Docker environment is sane and configures'
+    echo 'an environment file to use.'
+    echo
+    echo 'Optional flags:'
+    echo '  -f <filename>   use this file as the docker-compose config file'
+    echo '  -p <project>    use this name as the project prefix for docker-compose'
+}
 
-unset MANTA_PRIVATE_KEY
-#unset MANTA_KEY_ID
-unset NGINX_CONF
-unset NGINX_CONTAINERBUDDY
 
-export MANTA_PRIVATE_KEY=`cat manta.id_rsa`
-#MANTA_KEY_ID=`ssh-keygen -lf manta.id_rsa.pub | awk -F ' ' '{print $2}'`
-export NGINX_CONF=$(cat nginx/nginx.conf.ctmpl)
-#export NGINX_CONTAINERBUDDY=$(cat nginx/containerbuddy.json | tr --delete "\n")
+# default values which can be overriden by -f or -p flags
+export COMPOSE_PROJECT_NAME=wordpress
+export COMPOSE_FILE=
+
+# give the docker remote api more time before timeout
+export COMPOSE_HTTP_TIMEOUT=300
+
+# populated by `check` function whenever we're using Triton
+TRITON_USER=
+TRITON_DC=
+TRITON_ACCOUNT=
+
+# ---------------------------------------------------
+# Top-level commmands
+
+
+# Check for correct configuration
+check() {
+
+    command -v docker >/dev/null 2>&1 || {
+        echo
+        tput rev  # reverse
+        tput bold # bold
+        echo 'Docker is required, but does not appear to be installed.'
+        tput sgr0 # clear
+        echo 'See https://docs.joyent.com/public-cloud/api-access/docker'
+        exit 1
+    }
+    command -v json >/dev/null 2>&1 || {
+        echo
+        tput rev  # reverse
+        tput bold # bold
+        echo 'Error! JSON CLI tool is required, but does not appear to be installed.'
+        tput sgr0 # clear
+        echo 'See https://apidocs.joyent.com/cloudapi/#getting-started'
+        exit 1
+    }
+
+    # if we're not testing on Triton, don't bother checking Triton config
+    if [ ! -z "${COMPOSE_FILE}" ]; then
+        exit 0
+    fi
+
+    command -v triton >/dev/null 2>&1 || {
+        echo
+        tput rev  # reverse
+        tput bold # bold
+        echo 'Error! Joyent Triton CLI is required, but does not appear to be installed.'
+        tput sgr0 # clear
+        echo 'See https://www.joyent.com/blog/introducing-the-triton-command-line-tool'
+        exit 1
+    }
+
+    # make sure Docker client is pointed to the same place as the Triton client
+    local docker_user=$(docker info 2>&1 | awk -F": " '/SDCAccount:/{print $2}')
+    local docker_dc=$(echo $DOCKER_HOST | awk -F"/" '{print $3}' | awk -F'.' '{print $1}')
+    TRITON_USER=$(triton profile get | awk -F": " '/account:/{print $2}')
+    TRITON_DC=$(triton profile get | awk -F"/" '/url:/{print $3}' | awk -F'.' '{print $1}')
+    TRITON_ACCOUNT=$(triton account get | awk -F": " '/id:/{print $2}')
+    if [ ! "$docker_user" = "$TRITON_USER" ] || [ ! "$docker_dc" = "$TRITON_DC" ]; then
+        echo
+        tput rev  # reverse
+        tput bold # bold
+        echo 'Error! The Triton CLI configuration does not match the Docker CLI configuration.'
+        tput sgr0 # clear
+        echo
+        echo "Docker user: ${docker_user}"
+        echo "Triton user: ${TRITON_USER}"
+        echo "Docker data center: ${docker_dc}"
+        echo "Triton data center: ${TRITON_DC}"
+        exit 1
+    fi
+
+    local triton_cns_enabled=$(triton account get | awk -F": " '/cns/{print $2}')
+    if [ ! "true" == "$triton_cns_enabled" ]; then
+        echo
+        tput rev  # reverse
+        tput bold # bold
+        echo 'Error! Triton CNS is required and not enabled.'
+        tput sgr0 # clear
+        echo
+        exit 1
+    fi
+
+    if [ ! -f "_env" ]; then
+        echo "Creating a configuration file..."
+        cp _env.example _env
+        echo >> _env
+        echo MANTA_PRIVATE_KEY=`cat manta.id_rsa` >> _env
+        #echo MANTA_KEY_ID=`ssh-keygen -lf manta.id_rsa.pub | awk -F ' ' '{print $2}'` >> _env
+        echo NGINX_CONF=$(cat nginx/nginx.conf.ctmpl) >> _env
+        #echo NGINX_CONTAINERBUDDY=$(cat nginx/containerbuddy.json | tr --delete "\n") >> _env
+        echo 'Edit the _env file to configure your WordPress envrionment'
+    fi
+}
+
+# ---------------------------------------------------
+# parse arguments
+
+while getopts "f:p:h" optchar; do
+    case "${optchar}" in
+        f) export COMPOSE_FILE=${OPTARG} ;;
+        p) export COMPOSE_PROJECT_NAME=${OPTARG} ;;
+    esac
+done
+shift $(expr $OPTIND - 1 )
+
+until
+    cmd=$1
+    if [ ! -z "$cmd" ]; then
+        shift 1
+        $cmd "$@"
+        if [ $? == 127 ]; then
+            help
+        fi
+        exit
+    fi
+do
+    echo
+done
+
+# default behavior
+check
